0e228e044ec2af8dbf06f200170b42e3ea6ef614
Bennie <bennie@easysystems.co.za>
2016-11-23 13:48:58 +0200

    New button action

--- Default.aspx.vb
+++ Default.aspx.vb
@@ -5,4 +5,17 @@
 
     End Sub
 
+    Protected Sub btnNewButton_Click(sender As Object, e As EventArgs) Handles btnNewButton.Click
+
+        Dim x = 0
+
+        Dim newarray As New ArrayList
+
+        For Each item In newarray
+
+            Dim y = 0
+
+        Next
+
+    End Sub
 End Class
