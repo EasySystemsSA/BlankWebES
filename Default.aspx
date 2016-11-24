f61c516716df905ddb8dfe60fa39e0d84704b498
Bennie <bennie@easysystems.co.za>
2016-11-24 10:23:32 +0200

    Added JQuery and Bootstrap Scripts Styled Contact Us and Default a little bit.

--- Default.aspx
+++ Default.aspx
@@ -5,17 +5,27 @@
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head runat="server">
     <title></title>
+    <link href="Content/bootstrap.min.css" rel="stylesheet" />
+    <link href="Content/bootstrap-theme.min.css" rel="stylesheet" />
 </head>
 <body>
     <form id="form1" runat="server">
-    <div>
+    <div class="container">
+<asp:LinkButton ID="lnkGoToContactUs" runat="server" CssClass="btn btn-primary">CONTACT US</asp:LinkButton>
+        <h1>Playground</h1>
+        <br />
+        <br />
         <asp:Label Text="" ID="lblCal" runat="server" Font-Bold="true" ForeColor="Red"  />
         <br /><br />
         <asp:Button ID="btnNew2" runat="server" Text="Look at the Christmas lights!" />
         <br />
         <br />
-        <asp:LinkButton ID="lnkGoToContactUs" runat="server">CONTACT US</asp:LinkButton>
+        
     </div>
+
+        <script src="Scripts/jquery-3.1.1.min.js"></script>
+        <script src="Scripts/bootstrap.min.js"></script>
+
     </form>
 </body>
 </html>
--- Default.aspx.designer.vb
+++ Default.aspx.designer.vb
@@ -12,7 +12,7 @@ Option Explicit On
 
 
 Partial Public Class _Default
-
+    
     '''<summary>
     '''form1 control.
     '''</summary>
@@ -21,24 +21,7 @@ Partial Public Class _Default
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents form1 As Global.System.Web.UI.HtmlControls.HtmlForm
-
-    '''<summary>
-    '''lblCal control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents lblCal As Global.System.Web.UI.WebControls.Label
-
-    '''<summary>
-    '''btnNew2 control.
-    '''</summary>
-    '''<remarks>
-    '''Auto-generated field.
-    '''To modify move field declaration from designer file to code-behind file.
-    '''</remarks>
-    Protected WithEvents btnNew2 As Global.System.Web.UI.WebControls.Button
+    
     '''<summary>
     '''lnkGoToContactUs control.
     '''</summary>
@@ -47,4 +30,22 @@ Partial Public Class _Default
     '''To modify move field declaration from designer file to code-behind file.
     '''</remarks>
     Protected WithEvents lnkGoToContactUs As Global.System.Web.UI.WebControls.LinkButton
+    
+    '''<summary>
+    '''lblCal control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents lblCal As Global.System.Web.UI.WebControls.Label
+    
+    '''<summary>
+    '''btnNew2 control.
+    '''</summary>
+    '''<remarks>
+    '''Auto-generated field.
+    '''To modify move field declaration from designer file to code-behind file.
+    '''</remarks>
+    Protected WithEvents btnNew2 As Global.System.Web.UI.WebControls.Button
 End Class
